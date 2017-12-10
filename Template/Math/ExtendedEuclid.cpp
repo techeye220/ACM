ad092d944342223f3e7265e25b7e6816ad2eb340
Luo <anatasmloz@foxmail.com>
2017-12-10 12:01:51 +0800

    Math

---
+++ Template/Math/ExtendedEuclid.cpp
@@ -0,0 +1,31 @@
+//如果 GCD(a,b) GCD(a,b) GCD(a,b) GCD(a,b) = d, 则存在 x, y, 使 d = ax + by
+//extended_euclid(a, b) = ax + by
+
+#include <iostream>
+using namespace std;
+int extended_euclid(int a, int b, int &x, int & y)
+{
+	int d;
+	if(b == 0) {
+		x = 1;
+		y = 0;
+		return a;
+	}
+	d = extended_euclid(b, a % b, y, x);
+	y -= a / b * x;
+	return d;
+}
+
+int main()
+{
+	int a = 10, b = 3;
+	int x, y;
+	cout << extended_euclid(a, b, x, y) << endl;
+	cout << x << " " << y << endl;
+	return 0;
+}
+
+
+
+
+
--- Template/Math/Math.md
+++ Template/Math/Math.md
@@ -1,3 +1,5 @@
 + Greatest Common Divisor(最大公约数)(C++)
 + Prime (素数判断)(C++)
 + Subset (求大小为K的所有子集)(C++)
++ SlevePrime (素数筛法)(C++)
++ ExtendedEuclid（扩展欧几里得算法）(C++)
